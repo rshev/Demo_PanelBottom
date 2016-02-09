ee7eff48f7f2afa364aaffcd05c348fc697db26a
Roman Shevtsov <roman.shevtsov@gmail.com>
2016-02-09 12:33:37 +0100

    Initial Commit

---
+++ testPanelBottom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testPanelBottom
+//
+//  Created by asdfgh1 on 09/02/16.
+//  Copyright © 2016 rshev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
